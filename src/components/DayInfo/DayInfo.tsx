cdc8a89b70d8eebec9f9935e621964fe74d679cd
Luis <luis.oliveirabr1@gmail.com>
2023-09-14 12:30:44 -0300

    Componentizado Sabado

--- src/components/DayInfo/DayInfo.tsx
+++ src/components/DayInfo/DayInfo.tsx
@@ -3,14 +3,6 @@ import "./dayInfo.css";
 import panturrilhaLeg from "../../../public/Panturrilha-no-leg-press.webp";
 import panturrilhaAparelho from "../../../public/Panturrilha-em-pe-no-aparelho.webp";
 import panturrilhaEscada from "../../../public/Elevacao-de-panturrilha-em-pe-escada.webp";
-import crucifixo from "../../../public/crucifixo.webp";
-import supinoRetoHalter from "../../../public/supino-reto-com-halteres.webp";
-import supinoInclinadoHalter from "../../../public/supino-inclinado-com-halteres.webp";
-import voadorInclinado from "../../../public/voador-cabo-banco-inclinado.webp";
-import tricepsPulley from "../../../public/triceps-puxada-no-pulley.gif";
-import tricepsPulleyCabeca from "../../../public/triceps-no-pulley-atras-ca-cabeca.webp";
-import extencaoDeitado from "../../../public/extencao-triceps-deitado.webp";
-import tricepsCoice from "../../../public/triceps-coice.gif";
 import aducao from "../../../public/pernas-aducao-de-pernas-na-maquina.webp";
 import abducaoMaquina from "../../../public/abducao-de-pernas-na-maquina-com-cabos.webp";
 import agachamentoRack from "../../../public/agachamento-no-rack.webp";
@@ -23,6 +15,7 @@ import Segunda from "../Segunda/Segunda";
 import Terca from "../Terça/Terca";
 import Quarta from "../Quarta/Quarta";
 import Quinta from "../Quinta/Quinta";
+import Sexta from "../Sexta/Sexta";
 
 interface TypeProps {
   onPressSegunda: () => void;
@@ -42,10 +35,7 @@ interface TypeProps {
 function DayInfo(props: TypeProps) {
   const [page, setPage] = useState(1);
 
-  function closeSexta() {
-    props.onPressSexta();
-    setPage(1);
-  }
+
   function closeSabado() {
     props.onPressSabado();
     setPage(1);
@@ -97,133 +87,13 @@ function DayInfo(props: TypeProps) {
         />
       )}
       {props.sexta && (
-        <div className="containerWorkDay">
-          <div className="headerWorkDay">
-            <p className="marginHeader">Sexta</p>
-            <p>Peito - Triceps - Abdomem</p>
-            <button className="btnClose" onClick={closeSexta}>
-              X
-            </button>
-          </div>
-          <div className="container-exercises">
-            {page === 1 && (
-              <div className="title-exercises">
-                <p style={{ height: 40 }}>Peito</p>
-                <div className="exercises">
-                  <div>
-                    <div className="blocks">
-                      <p>Supino reto com halteres</p>
-                      <img
-                        src={supinoRetoHalter}
-                        className="img-exercices"
-                      ></img>
-                    </div>
-                    <div className="blocks">
-                      <p>Supino inclinado com halteres</p>
-                      <img
-                        src={supinoInclinadoHalter}
-                        className="img-exercices"
-                      ></img>
-                    </div>
-                  </div>
-                  <div className="next" onClick={nextExercise}>
-                    <img
-                      src="../../../public/arrow.png"
-                      alt="Seta avançar"
-                    ></img>
-                  </div>
-                  <div>
-                    <div className="blocks">
-                      <p>Crucifixo</p>
-                      <img src={crucifixo} className="img-exercices"></img>
-                    </div>
-                    <div className="blocks">
-                      <p>Voador no cabo com banco inclinado</p>
-                      <img
-                        src={voadorInclinado}
-                        className="img-exercices"
-                      ></img>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            )}
-            {page === 2 && (
-              <div className="title-exercises">
-                <p style={{ height: 40 }}>Triceps</p>
-                <div className="exercises">
-                  <div>
-                    <div className="blocks">
-                      <p>Tríceps na polia alta com barra reta</p>
-                      <img src={tricepsPulley} className="img-exercices"></img>
-                    </div>
-                    <div className="blocks">
-                      <p>Extensão de tríceps no cabo sob a cabeça com corda</p>
-                      <img
-                        src={tricepsPulleyCabeca}
-                        className="img-exercices"
-                      ></img>
-                    </div>
-                  </div>
-                  <div className="next" onClick={nextExercise}>
-                    <img
-                      src="../../../public/arrow.png"
-                      alt="Seta avançar"
-                    ></img>
-                  </div>
-                  <div className="prev" onClick={previousExercise}>
-                    <img
-                      src="../../../public/arrow.png"
-                      alt="Seta voltar"
-                    ></img>
-                  </div>
-                  <div>
-                    <div className="blocks">
-                      <p>Extensão de tríceps deitado</p>
-                      <img
-                        src={extencaoDeitado}
-                        className="img-exercices"
-                      ></img>
-                    </div>
-                    <div className="blocks">
-                      <p>Triceps Coice</p>
-                      <img src={tricepsCoice} className="img-exercices"></img>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            )}
-            {page === 3 && (
-              <div className="title-exercises">
-                <p style={{ height: 40 }}>Abdomem</p>
-                <div className="exercises">
-                  <div>
-                    <div className="blocks">
-                      <p>Abdomem1</p>
-                    </div>
-                    <div className="blocks">
-                      <p>Abdomem2</p>
-                    </div>
-                  </div>
-                  <div className="prev" onClick={previousExercise}>
-                    <img
-                      src="../../../public/arrow.png"
-                      alt="Seta voltar"
-                    ></img>
-                  </div>
-                  <div>
-                    <div className="blocks">
-                      <p>Abdomem3</p>
-                    </div>
-                    <div className="blocks">
-                      <p>Abdomem4</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            )}
-          </div>
-        </div>
+        <Sexta
+          onPressSexta={props.onPressSexta}
+          setPage={setPage}
+          page={page}
+          nextExercise={nextExercise}
+          previousExercise={previousExercise}
+        />
       )}
       {props.sabado && (
         <div className="containerWorkDay">
