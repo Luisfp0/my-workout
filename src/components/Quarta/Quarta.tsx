c3d07100595c8e2c9d536d68b4a7181e05d69e3d
Luis <luis.oliveirabr1@gmail.com>
2023-10-03 11:33:10 -0300

    Finalizado quarta gluteos

--- src/components/Quarta/Quarta.tsx
+++ src/components/Quarta/Quarta.tsx
@@ -5,7 +5,11 @@ import abducaoMaquina from "../../../public/abducao-de-pernas-na-maquina-com-cab
 import agachamentoRack from "../../../public/agachamento-no-rack.webp";
 import leg45 from "../../../public/pernas-leg-press-45-tradicional.webp";
 import cadeiraExtensora from "../../../public/cadeira-extensora-extensora.gif";
+import explCadeiraExtensora from "../../../public/expl-quadriceps-lowres.webp";
+
 import cadeiraFlexora from "../../../public/cadeira-flexora.gif";
+import explCadeiraFlexora from "../../../public/expl-Isquiotibiais.jpg";
+
 import levantamentoTerra from "../../../public/levantamento-terra-com-barra.webp";
 import elevacaoPelvica from "../../../public/elevacao-pelvica.gif";
 import panturrilhaLeg from "../../../public/Panturrilha-no-leg-press.webp";
@@ -125,17 +129,19 @@ function Quarta(props: PropsQuarta) {
       ],
       errors: [
         {
-          title:
-            "Estender os joelhos na fase concêntrica do exercício",
-          description: "Na fase concêntrica, isto é, no momento de subir para a posição inicial, evite esticar os joelhos. Ao estender os joelhos, você acaba dando tempo para os membros inferiores descansarem, reduzindo a eficiência do exercício. ",
+          title: "Estender os joelhos na fase concêntrica do exercício",
+          description:
+            "Na fase concêntrica, isto é, no momento de subir para a posição inicial, evite esticar os joelhos. Ao estender os joelhos, você acaba dando tempo para os membros inferiores descansarem, reduzindo a eficiência do exercício. ",
         },
         {
           title: "Não encaixar os ombros",
-          description: "Os ombros precisam estar encaixados abaixo do apoio o tempo todo. Além disso, abra bem o peito e puxe os ombros para trás antes de começar o exercício..",
+          description:
+            "Os ombros precisam estar encaixados abaixo do apoio o tempo todo. Além disso, abra bem o peito e puxe os ombros para trás antes de começar o exercício..",
         },
         {
           title: "Posicionar os pés incorretamente",
-          description: "Para praticar o agachamento no hack de forma segura, é importante posicionar os pés na mesma largura dos ombros e deixá-los centralizados na plataforma. Os pés podem ficar um pouco à frente do corpo, mas não coloque-os na parte superior da plataforma para não sobrecarregar os quadríceps.",
+          description:
+            "Para praticar o agachamento no hack de forma segura, é importante posicionar os pés na mesma largura dos ombros e deixá-los centralizados na plataforma. Os pés podem ficar um pouco à frente do corpo, mas não coloque-os na parte superior da plataforma para não sobrecarregar os quadríceps.",
         },
       ],
       onClose: () => setExplicacaoAberta(null),
@@ -148,19 +154,27 @@ function Quarta(props: PropsQuarta) {
       weight: 0,
       addWeight: () => addWeight(3),
       removeWeight: () => removeWeight(3),
-      texts: ["Text1", "Text2", "Text3"],
+      texts: [
+        "A maneira correta de fazer o leg press 45 graus tradicional começa com o posicionamento adequado das costas, glúteos e braços no banco inclinado. Você deve sentar-se no banco, de forma que as suas costas fiquem bem apoiadas no encosto, assim como os glúteos. Os braços devem ficar ao lado do corpo. ",
+        "Então, apoie os pés na plataforma afastados entre si na largura dos quadris. Dessa forma, os quadríceps serão os músculos mais trabalhados durante o exercício. Também é importante deixar os pés levemente voltados para fora, para evitar que os joelhos avancem para a linha média do corpo.",
+        "Em seguida, empurre a plataforma com os pés até quase esticar os joelhos. Mantenha essa posição enquanto solta a trava e se prepara para começar o exercício. ",
+        "Flexione os joelhos sem encostá-los no peito enquanto deixa a plataforma descer lentamente. Logo depois, empurre a plataforma para cima mais uma vez. Repita o movimento até completar a sua série.",
+      ],
       errors: [
         {
-          title: "error1",
-          description: "error1",
+          title: "Não apoiar as costas no encosto",
+          description:
+            "É preciso manter as costas apoiadas no encosto o tempo todo. Aliás, contrair o abdômen para preservar a curvatura natural da coluna e deixá-la bem apoiada ajuda a evitar lesões e dores na lombar e mantém o foco nos músculos das pernas e nos glúteos.",
         },
         {
-          title: "error1",
-          description: "error1.",
+          title: "Levantar os calcanhares",
+          description:
+            "No leg press tradicional não é indicado ficar apenas com as pontas dos pés na plataforma. De fato, é essencial manter todo o pé apoiado para não sobrecarregar os joelhos.",
         },
         {
-          title: "error1",
-          description: "error1",
+          title: "Esticar totalmente o joelho",
+          description:
+            "Não estique totalmente os joelhos ao empurrar a plataforma do leg press, pois isso aumenta o risco de lesões. Além disso, manter os joelhos levemente flexionados faz com que a tensão muscular se concentre nos quadríceps.",
         },
       ],
       onClose: () => setExplicacaoAberta(null),
@@ -169,23 +183,32 @@ function Quarta(props: PropsQuarta) {
       tittleApresentation: "Cadeira extensora",
       imageApresentation: cadeiraExtensora,
       title: "Cadeira extensora",
-      imageJpg: cadeiraExtensora,
+      imageJpg: explCadeiraExtensora,
       weight: 0,
       addWeight: () => addWeight(4),
       removeWeight: () => removeWeight(4),
-      texts: ["Text1", "Text2", "Text3"],
+      texts: [
+        "Para garantir o ajuste correto, certifique-se de que a barra acolchoada esteja logo acima dos seus tornozelos em uma posição confortável. Além disso, seus joelhos devem ficar em um ângulo de 90 graus.",
+        "Então, apoie a lombar no encosto do banco e deixe as mãos ao lado do corpo. Os pés devem ficar levemente afastados entre si e os dedos apontando para a frente. ",
+        "Por fim, verifique se o ajuste permite que você estenda suas pernas sem afetar a postura. ",
+        "Para iniciar o exercício, ative o abdômen e levante a barra acolchoada sobre os tornozelos até estender as pernas, mas sem travar os joelhos. Faça uma pequena pausa no topo e retorne lentamente à posição inicial.",
+        "A respiração também ajuda a ativar os músculos corretamente. Dessa forma, expire o ar enquanto levanta a barra e inspire enquanto baixa as pernas. Não esqueça de focar o exercício nos membros inferiores, sem retirar a parte superior do corpo do banco durante a execução. ",
+      ],
       errors: [
         {
-          title: "error1",
-          description: "error1",
+          title: "Travar os joelhos",
+          description:
+            "No momento de fazer a extensão de pernas na máquina, evite estender totalmente as pernas. Fazer isso pode colocar muita tensão nos joelhos e esticar as articulações locais, causando dores e aumentando o risco de lesões. ",
         },
         {
-          title: "error1",
-          description: "error1.",
+          title: "Levantar mais peso do que aguenta",
+          description:
+            "Colocar muita carga na máquina pode aumentar o risco de lesão. De fato, exagerar no peso eleva o risco de distensão dos ligamentos dos joelhos. Além disso, o excesso de carga pode prejudicar a saúde das articulações dos tornozelos..",
         },
         {
-          title: "error1",
-          description: "error1",
+          title: "Movimentar as pernas rapidamente",
+          description:
+            "Realizar o movimento muito rapidamente diminui o tempo que o músculo fica sob tensão. Isso faz com que a musculatura não seja ativada corretamente durante o exercício.",
         },
       ],
       onClose: () => setExplicacaoAberta(null),
@@ -194,23 +217,30 @@ function Quarta(props: PropsQuarta) {
       tittleApresentation: "Cadeira flexora",
       imageApresentation: cadeiraFlexora,
       title: "Cadeira flexora",
-      imageJpg: cadeiraFlexora,
+      imageJpg: explCadeiraFlexora,
       weight: 0,
       addWeight: () => addWeight(5),
       removeWeight: () => removeWeight(5),
-      texts: ["Text1", "Text2", "Text3"],
+      texts: [
+        "omece ajustando a máquina de acordo com a sua altura. Para isso, deite-se de bruços na mesa flexora e veja se seus tornozelos alcançam a barra acolchoada. Ajuste de modo que a almofada fique entre os calcanhares e as panturrilhas.",
+        "Então, arrume seu posicionamento para iniciar o exercício. Deixe os pés separados entre si por alguns centímetros, deite de barriga para baixo apoiando o tronco na parte de cima do banco e as coxas na parte inferior, que é mais inclinada. Coloque as mãos nos apoios laterais e, enfim, ajuste a carga desejada.",
+        "Solte o ar enquanto levanta a barra com as pernas em direção aos glúteos. Faça uma pausa no topo. Inspire o ar voltando lentamente para a posição inicial.",
+      ],
       errors: [
         {
-          title: "error1",
-          description: "error1",
+          title: "Retirar o tronco ou os quadris do banco",
+          description:
+            "Os quadris e a parte superior do corpo devem ficar imóveis durante todo o exercício, pois só assim é possível ter certeza de que os isquiotibiais estão sendo estimulados. ",
         },
         {
-          title: "error1",
-          description: "error1.",
+          title: "Arquear as costas",
+          description:
+            "É importante que as costas fiquem retas, pois apesar de curvar as costas parecer facilitar o movimento, fazer isso o torna ineficaz e ainda aumenta o risco de lesões. .",
         },
         {
-          title: "error1",
-          description: "error1",
+          title: "Levantar a cabeça",
+          description:
+            "Você deve permanecer o tempo todo com o pescoço alinhado com o resto da coluna e com o olhar para baixo. Ao levantar a cabeça e olhar para a frente, você coloca uma tensão desnecessária na cervical.",
         },
       ],
       onClose: () => setExplicacaoAberta(null),
@@ -223,19 +253,28 @@ function Quarta(props: PropsQuarta) {
       weight: 0,
       addWeight: () => addWeight(6),
       removeWeight: () => removeWeight(6),
-      texts: ["Text1", "Text2", "Text3"],
+      texts: [
+        "É essencial que você mantenha a postura ereta e, se possível, olhando para a frente. Assim, comece ajustando a sua postura. Primeiramente, afaste as pernas mantendo-as na largura dos ombros. Em seguida, flexione levemente os joelhos e mantenha as costas retas, com quadris, ombros e cabeça alinhados. ",
+        "Então, faça um agachamento para tirar a barra do chão, suba estendendo os joelhos e trazendo a barra bem próxima ao corpo.",
+        "Agora, agache novamente deslizando a barra rente ao corpo e movimentando o bumbum para trás, como se fosse sentar em uma cadeira. O tronco deve ficar levemente inclinado para a frente com as costas retas e paralelas ao chão. Além disso, os joelhos devem continuar flexionados sem ultrapassar as pontas dos pés.",
+        "Sem colocar a barra de volta no chão, repita o movimento até completar a sua série.",
+        "Outra dica interessante é contrair o abdômen, pois isso estabiliza e protege a sua coluna durante o levantamento terra.",
+      ],
       errors: [
         {
-          title: "error1",
-          description: "error1",
+          title: "Curvar a coluna",
+          description:
+            "A coluna deve se manter ereta durante todo o movimento. Ao curvar a coluna, você leva os ombros para a frente, prejudicando assim a postura e o equilíbrio durante a execução.",
         },
         {
-          title: "error1",
-          description: "error1.",
+          title: "Manter a barra muito longe do corpo",
+          description:
+            "Se você afastar a barra do seu corpo, o exercício se torna menos eficaz. Além disso, perde-se o alinhamento natural da coluna, o que provoca desequilíbrio e sobrecarga da região lombar..",
         },
         {
-          title: "error1",
-          description: "error1",
+          title: "Não deslocar os quadris para trás",
+          description:
+            "Durante o agachamento, é fundamental movimentar os quadris para trás, para não exigir demais das articulações do joelho. Isso também ajuda no equilíbrio e na ativação dos glúteos e isquiotibiais, envolvidos no levantamento terra.",
         },
       ],
       onClose: () => setExplicacaoAberta(null),
@@ -248,20 +287,13 @@ function Quarta(props: PropsQuarta) {
       weight: 0,
       addWeight: () => addWeight(7),
       removeWeight: () => removeWeight(7),
-      texts: ["Text1", "Text2", "Text3"],
-      errors: [
-        {
-          title: "error1",
-          description: "error1",
-        },
-        {
-          title: "error1",
-          description: "error1.",
-        },
-        {
-          title: "error1",
-          description: "error1",
-        },
+      texts: [
+        "Você pode utilizar um colchonete para o seu conforto.",
+        "Anilhas, caneleiras e halteres podem ser posicionados sobre sua pelve, dessa forma os treinos podem se tornar mais intensos e os resultados mais rápidos.",
+        "Deite-se sobre o solo e alinhe os braços ao lado do corpo.",
+        "Flexione levemente as pernas sem interromper o contato das solas dos pés com o chão.",
+        "Inspire e eleve a pelve até a coluna perder o contato com o solo. Eleve a pelve o máximo que conseguir e contraia os glúteos. Mantenha-se nessa posição por 5-10 segundos",
+        "Retorne à posição inicial e repita os movimentos.",
       ],
       onClose: () => setExplicacaoAberta(null),
     },
